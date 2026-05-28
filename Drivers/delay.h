a9218d19c9268aa20be6004c06aa7e9946f4d961
rohithyalldasari <rohithyalldasari@gmail.com>
2026-05-28 11:27:13 +0530

    Create delay.h

---
+++ Drivers/delay.h
@@ -0,0 +1,8 @@
+#ifndef __DELAY_H__
+#define __DELAY_H__
+
+// Software timing driver functions for ARM7 execution cycles
+void Delay_us(unsigned int count);
+void Delay_ms(unsigned int count);
+
+#endif
